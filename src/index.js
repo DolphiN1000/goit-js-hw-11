f13aeb42232ee5bcbe2ce6d109453e7517b43d2d
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2022-12-22 22:00:52 +0200

    123

--- src/index.js
+++ src/index.js
@@ -2,28 +2,31 @@
 import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import addBody from './body';
+import addBody from './js/body';
+import './js/pixabay-api'
 addBody();
-const { height: cardHeight } = document
-  .querySelector('.gallery')
-  .firstElementChild.getBoundingClientRect();
 
-window.scrollBy({
-  top: cardHeight * 2,
-  behavior: 'smooth',
-});
+const refs = {
+  inputForm: document.querySelector('#search-form'),
+  loadMoreBtn: document.querySelector('.load-more'),
+}
+
+refs.inputForm.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+}
+
+// const { height: cardHeight } = document
+//   .querySelector('.gallery')
+//   .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: 'smooth',
+// });
 
 
-var API_KEY = '32256476-11f87d3a9f8518386821fa1a7';
-var URL =
-  'https://pixabay.com/api/?key=' +
-  API_KEY +
-  '&q=' +
-  encodeURIComponent('red roses');
-$.getJSON(URL, function (data) {
-  if (parseInt(data.totalHits) > 0)
-    $.each(data.hits, function (i, hit) {
-      console.log(hit.pageURL);
-    });
-  else console.log('No hits');
-});
+
+console.log(URL)
